1f691ae3a660b6744436f3c946166ed421b473ff
Carlos David Mendoza <carlosmendoza1719@gmail.com>
2022-02-28 23:20:48 -0600

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Paletas } from './paletas';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer, //reducer
-        initialState, //our initial state
+        combineReducers({
+            paletas: Paletas,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
---
+++ src/redux/paletas.js
@@ -0,0 +1,8 @@
+import { PALETAS } from '../shared/paletas';
+
+export const Paletas = (state = PALETAS, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
